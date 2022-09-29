a580a23b2a4aaa418b0383d7733f087669c070a5
jaclog05 <jaidercueto.1010@gmail.com>
2022-09-29 11:48:41 -0500

    homework 04-AJAX

---
+++ 04-Ajax/homework/index.js
@@ -0,0 +1,37 @@
+$('#boton').click(function(){
+    $('#lista').remove()
+    $(`<ul id="lista"></ul>`).insertBefore('h2')
+    $.get('http://localhost:5000/amigos', function(data){
+        data.forEach(element => {
+            $(`<li>${element.name}</li>`).appendTo('ul')
+        });
+    })
+    $('#amigo').text("")
+    $('#success').text('')
+})
+
+$('#search').click(function(){
+    let input = $('#input')
+    $.get(`http://localhost:5000/amigos/${input[0].value}`, function(data){
+        $('#amigo').text(data.name)
+    })
+    input[0].value = ""
+})
+
+$('#delete').click(function(){
+    let inputDelete = $('#inputDelete')
+    // $.delete(`http://localhost:5000/amigos/${inputDelete[0].value}`, function(data){
+    //     $('#success').text('Borrado con exito')
+    // })
+
+    $.ajax({
+        url: `http://localhost:5000/amigos/${inputDelete[0].value}`,
+        type: 'DELETE',
+        dataType: 'json',
+        success: function(data) {
+            $('#success').text('Borrado con exito');
+        },
+    });
+
+    inputDelete[0].value = ""
+})
