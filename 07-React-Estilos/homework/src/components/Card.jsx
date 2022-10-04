4a649170b7cba024e8a1c40f56a107967b016a1e
jaclog05 <jaidercueto.1010@gmail.com>
2022-10-04 11:11:33 -0500

    07 React-styles homework

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import styles from './Card.module.css'
 
-export default function Card(props) {
+export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div className={styles.component}>
+      <div className={styles.titleAndButtonWrapper}>
+        <h2>{name}</h2>
+        <button className={styles.button} onClick={onClose}>×</button>
+      </div>
+      <h4>Min: {min}</h4>
+      <h4>Max: {max}</h4>
+      <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt=""/>
+    </div>
+  )
 };
