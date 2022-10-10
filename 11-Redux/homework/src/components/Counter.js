9e8ac877db49043b0ed33601ba860422d27bf5d2
jaclog05 <jaidercueto.1010@gmail.com>
2022-10-10 15:29:36 -0500

    11 redux hw

--- 11-Redux/homework/src/components/Counter.js
+++ 11-Redux/homework/src/components/Counter.js
@@ -15,13 +15,14 @@ class Counter extends Component {
     render() {
         // Completa las funciones onClick de los botones
         // Al hacer clic en estos botones, el recuento debe disminuir o aumentar en consecuencia
+        console.log(this.props)
         return (
             <p>
                 Clickeado: {this.props.count} veces
-                <button onClick={() => {/* Completar */ }}>
+                <button onClick={() => this.props.increment()}>
                     + {/* Incremeta */}
                 </button>
-                <button onClick={() => {/* Completar */ }}>
+                <button onClick={() => this.props.decrement()}>
                     -  {/* Decrementa */}
                 </button>
                  {/* Si quieres hacer los extra credit puede descomentar las lineas de abajo */}
